7779f97c5cc32ea54d281b68d0f51b9ba513637e
HynNan <nhanbeta000@gmail.com>
2025-11-25 21:14:23 +0700

    Thêm code Sever_RPS_PVP và Client_RPS_PVP phần giữa kỳ

---
+++ BTL_GK/K_B_B_PVP/Client_RPS_PVP.py
@@ -0,0 +1,27 @@
+# ================================
+# Import toàn bộ thư viện
+# Khởi tạo socket client
+# Kết nối đến server
+# Hiển thị trạng thái đầu tiên
+# ================================
+
+import socket
+import threading
+import tkinter as tk
+from tkinter import messagebox
+
+HOST = "127.0.0.1"      # đổi sang IP server khi chơi LAN
+PORT = 7000
+
+class RPSClientGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Kéo Búa Bao – PVP")
+
+        self.s = socket.socket()
+        try:
+            self.s.connect((HOST, PORT))
+        except:
+            messagebox.showerror("Lỗi", "Không kết nối được tới server!")
+            root.destroy()
+            return
